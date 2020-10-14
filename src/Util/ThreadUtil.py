46fe9a2f34bc4c569dbec9a54241181ab661bee1
俭锋 梁 <2402901363@qq.com>
2020-10-14 21:33:21 +0800

    修改了一些小bug

--- src/Util/ThreadUtil.py
+++ src/Util/ThreadUtil.py
@@ -16,20 +16,23 @@ class ThreadUtil:
         # 获取URL中对应的所有工作的具体信息
         print("开始获取"+jobUrl+"的工作信息列表\n")
         jobList = JobUtil.getJobList(jobUrl)
-        print("完成获取" + jobUrl + "的工作信息列表\n")
-        # 将list数组转换为json格式的数组
-        jobListJsonStr = JsonUtil.listToJson(jobList)
-        jobDesStr = JsonUtil.getJobListDesStr(jobList)
-        # print(jobListJsonStr)
-        # 保存文本到指定的文件中去
-        if os.path.isdir(saveFileDirPath):
-            # 将工作具体的信息数据以json格式保存到指定文件的文件名
-            jsonFilePath = saveFileDirPath + '/' + fileName + '.json'
-            # 将工作岗位要求和描述以txt格式保存到指定文件的文件名
-            desFilePath = saveFileDirPath + '/' + fileName + '.txt'
-            # 将工作信息保到json文件中
-            FileUtil.saveTextToFile(text=jobListJsonStr,filePath=jsonFilePath)
-            # 将工作描述要求保存到txt文件中去
-            FileUtil.saveTextToFile(text=jobDesStr, filePath=desFilePath)
-            print("保存文件"+fileName+"成功。。。。\n")
+        if len(jobList) == 0:
+            print('爬到'+jobUrl+'的数据为空\n')
+        else:
+            print("完成获取" + jobUrl + "的工作信息列表\n")
+            # 将list数组转换为json格式的数组
+            jobListJsonStr = JsonUtil.listToJson(jobList)
+            jobDesStr = JsonUtil.getJobListDesStr(jobList)
+            # print(jobListJsonStr)
+            # 保存文本到指定的文件中去
+            if os.path.isdir(saveFileDirPath):
+                # 将工作具体的信息数据以json格式保存到指定文件的文件名
+                jsonFilePath = saveFileDirPath + '/' + fileName + '.json'
+                # 将工作岗位要求和描述以txt格式保存到指定文件的文件名
+                desFilePath = saveFileDirPath + '/' + fileName + '.txt'
+                # 将工作信息保到json文件中
+                FileUtil.saveTextToFile(text=jobListJsonStr, filePath=jsonFilePath)
+                # 将工作描述要求保存到txt文件中去
+                FileUtil.saveTextToFile(text=jobDesStr, filePath=desFilePath)
+                print("保存文件" + fileName + "成功。。。。\n")
         return jobList
